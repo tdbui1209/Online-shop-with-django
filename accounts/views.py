7d071b14c9a098843822611935ecb5cb6b9c70e0
tdbui1209 <tdbui1209@gmail.com>
2023-08-04 09:36:10 +0700

    add view for login, signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignupForm
+from django.contrib.auth import login as auth_login  # to avoid conflict with login function in views.py
 
-# Create your views here.
+
+def login(request, user):
+    return render(request, 'registration/login.html', {'user': user})
+
+def signup(request):
+    if request.method == 'GET':
+        form = SignupForm()
+        return render(request, 'registration/signup.html', {'form': form})
+    elif request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('shop:product_list')
+        else:
+            return render(request, 'registration/signup.html', {'form': form})
