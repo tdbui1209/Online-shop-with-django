80fc0713a8c40c3f752e20d3a32a1160d63eea88
tdbui1209 <tdbui1209@gmail.com>
2023-08-04 18:21:14 +0700

    add view orders of user:

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.contrib.auth import login as auth_login  # to avoid conflict with login function in views.py
+from orders.models import Order, OrderItem
 
 
 def login(request, user):
@@ -18,3 +19,14 @@ def signup(request):
             return redirect('shop:product_list')
         else:
             return render(request, 'registration/signup.html', {'form': form})
+
+def orders(request):
+    orders = Order.objects.filter(user=request.user)
+    orderitems = OrderItem.objects.filter(order__in=orders)
+    orderitems_total_dict = {}  # to calculate total cost of each order
+    for orderitem in orderitems:
+        if orderitem.order.id in orderitems_total_dict:
+            orderitems_total_dict[orderitem.order.id] += orderitem.get_cost()
+        else:
+            orderitems_total_dict[orderitem.order.id] = orderitem.get_cost()
+    return render(request, 'accounts/orders.html', {'orders': orders, 'orderitems': orderitems, 'orderitems_total_dict': orderitems_total_dict})
