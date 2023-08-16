2391925585aa8c73dfaf9a855b7444619f0029fa
tdbui1209 <tdbui1209@gmail.com>
2023-08-16 11:08:56 +0700

    change quantity form to IntegerField

--- cart/forms.py
+++ cart/forms.py
@@ -4,5 +4,5 @@ from django import forms
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    quantity = forms.IntegerField(min_value=1, step_size=1, initial=1)
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
